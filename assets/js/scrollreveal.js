226522318f6e398d9cb4a4450395babb21709df2
Gustavo Nascimento Souza <gustavo.ns@outlook.com.br>
2022-05-15 16:06:50 -0300

    :pencil: Added Services, About, Specialties and their Responsiveness

--- assets/js/scrollreveal.js
+++ assets/js/scrollreveal.js
@@ -9,12 +9,12 @@ const sr = ScrollReveal({
 sr.reveal('.nav-logo', { delay: 100 })
 sr.reveal('.nav-links', { delay: 100 })
 sr.reveal('.nav-link', { delay: 200, interval: 100 });
-sr.reveal('.scheduling-link', { delay: 1000 });
+sr.reveal('#schedule', { delay: 700 });
 
 //HOME
-sr.reveal('.home-title', { delay: 100, reset: false });
-sr.reveal('.home-subtitle', { delay: 1100, reset: false });
-sr.reveal('.home-text', { delay: 2100, reset: false });
-sr.reveal('.home-link', { delay: 3100, reset: false });
-sr.reveal('.home-img', { delay: 4100, reset: false });
-sr.reveal('.home-stats', { delay: 4100, reset: false });
+sr.reveal('#home-title', { delay: 100, reset: false });
+sr.reveal('#home-subtitle', { delay: 1100, reset: false });
+sr.reveal('#home-text', { delay: 2100, reset: false });
+sr.reveal('#home-link', { delay: 3100, reset: false });
+sr.reveal('#home-img', { delay: 4100, reset: false });
+sr.reveal('#home-stats', { delay: 4100, reset: false });
